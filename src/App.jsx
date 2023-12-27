e034c8f998ebd5de4570561ce96e7c5c77907ebc
rahul patil <rahulpatil2387@gmail.com>
2023-12-27 20:38:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <h4>This is my 1st React Application.</h4>
+      
+    </div>
+  );
+}
+
+export default App;
